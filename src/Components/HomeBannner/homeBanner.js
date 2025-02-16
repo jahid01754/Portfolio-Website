af3b5355a59c175d52edab70c380cbe44a612d16
Jahid <jahidulhasan01754@gmail.com>
2025-02-16 15:33:19 +0600

    previous

--- src/Components/HomeBannner/homeBanner.js
+++ src/Components/HomeBannner/homeBanner.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 export default function HomeBanner() {
   return (
     <>
@@ -12,8 +13,8 @@ export default function HomeBanner() {
             className="absolute top-36 left-24  animate-upDownFloat"
           />
         </div>
-        <div className="flex justify-center items-center order-2 lg:order1 mt-10">
-          <div className="flex flex-row">
+        <div className="flex flex-col justify-center items-center order-2 lg:order1 mt-10">
+          <div className="container flex flex-row items-center justify-start w-full">
             <Image
               src="/pic/orangeDot.png"
               alt="Orange Dot"
@@ -24,8 +25,33 @@ export default function HomeBanner() {
 
             <h1 className="text-xl">I am a Frontend Developer</h1>
           </div>
-          <div></div>
-          <div></div>
+          <div className="container mt-4 flex flex-row">
+            <p className="text-black text-[50px] font-bold">
+              Creative Design and Web
+            </p>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                " Design",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                " Solutions",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              // style={{ fontSize: "2em", display: "inline-block" }}
+              className=" text-[50px] md:text-4xl font-bold text-blue-600 inline-block"
+              repeat={Infinity}
+            />
+          </div>
+          <div>
+            <p>
+              Nam libero tempore, cum soluta nobis est eligendi optio cumque
+              nihil impedit quo minus id quod maxime placeat facere possimus,
+              omnis voluptas assumenda est, omnis dolor repellendus. Temporibus
+              autem quibusdam et.
+            </p>
+          </div>
         </div>
 
         <div className="flex justify-center items-center relative order-1 lg:order-2">

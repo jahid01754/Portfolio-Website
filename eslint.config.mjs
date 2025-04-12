f031b4809a1d27f82339f437bf73e1ce4341361f
Jahid <jahidulhasan01754@gmail.com>
2025-04-12 19:38:33 +0600

    eslint problem

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+// module.exports = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default eslintConfig;
 
 module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  extends: ["next/core-web-vitals"],
 };
-
-export default eslintConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: false, // or remove this line entirely
+  },
+};

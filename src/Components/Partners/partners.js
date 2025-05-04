3dbc4e818618258dc72281624c08b4ceedd1037c
Jahid <jahidulhasan01754@gmail.com>
2025-05-04 12:53:02 +0600

    eslint issues

--- src/Components/Partners/partners.js
+++ src/Components/Partners/partners.js
@@ -91,7 +91,6 @@ export default function Partners() {
                       alt="Partner Icon"
                       width={100}
                       height={100}
-                      className=""
                     />
                   </div>
                 </SwiperSlide>
